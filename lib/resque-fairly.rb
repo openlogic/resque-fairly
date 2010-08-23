65f6456177b5762aab30c2ccf84e3fa2640c3b45
Peter Williams <pezra@barelyenough.org>
2010-08-22 21:24:53 -0600

    Implement fair scheduling behavior

---
+++ lib/resque-fairly.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'resque'
+
+require File.expand_path('resque/plugins/fairly', File.dirname(__FILE__))
+
+
---
+++ lib/resque/plugins/fairly.rb
@@ -0,0 +1,27 @@
+require 'resque/worker'
+
+module Resque::Plugins
+  module Fairly
+    # Returns a list of queues to use when searching for a job.  A
+    # splat ("*") means you want every queue 
+    #
+    # The queues will be ordered randomly and the order will change
+    # with every call.  This prevents any particular queue for being
+    # starved.  Workers will process queues in a random order each
+    # time the poll for new work.
+    def queues_randomly_ordered
+      queues_alpha_ordered.sort_by{rand}
+    end
+
+    def self.included(klass)
+      klass.instance_eval do 
+        alias_method :queues_alpha_ordered, :queues
+        alias_method :queues, :queues_randomly_ordered
+      end
+    end
+  end
+
+  Resque::Worker.send(:include, Fairly)
+end
+
+
