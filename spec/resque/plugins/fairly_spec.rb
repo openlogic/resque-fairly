9977b62dc6815963fb36f10bc2c65dfd9259b1de
Peter Williams <pezra@barelyenough.org>
2010-08-22 21:29:11 -0600

    fixed typo

--- spec/resque/plugins/fairly_spec.rb
+++ spec/resque/plugins/fairly_spec.rb
@@ -1,14 +1,14 @@
 require File.expand_path("../../spec_helper", File.dirname(__FILE__))
 
 describe Resque::Plugins::Fairly do
-  it "changes Resque::Worker#queues to return queues in a random order (rand seeded w/ 2" do
+  it "changes Resque::Worker#queues to return queues in a random order (rand seeded w/ 2)" do
     worker = Resque::Worker.new('a','b')
 
     srand(2) 
     worker.queues.should == ['b', 'a']
   end
 
-  it "changes Resque::Worker#queues to return queues in a random order (rand seeded w/ 1" do
+  it "changes Resque::Worker#queues to return queues in a random order (rand seeded w/ 1)" do
     worker = Resque::Worker.new('a','b')
 
     srand(1)
